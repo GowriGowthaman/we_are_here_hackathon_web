f53ced3b37638837c3904318bcb07a3d9b988eb5
GowriGowthaman <gowri@tartlabs.com>
2021-11-19 13:47:02 +0530

    loader added

---
+++ src/components/shared/FullPageLoader.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const FullPageLoader = () => {
+    return (
+        <div className="black_bg ">
+            <div className="loader" />
+        </div>
+    );
+};
